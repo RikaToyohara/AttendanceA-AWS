1fd7428af7fa1c6cb792602f286bd7b4742421a8
RikaToyohara <rikachan814sun@gmail.com>
2020-11-23 02:40:33 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+      base_title
+    else
+      page_name + " | " + base_title # 文字列を連結して返す
+    end
+  end
 end
